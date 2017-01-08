242bd8e97f36755d83e0017c399d9fa1178c3be2
MatthewRock <maliszmat@gmail.com>
2017-01-09 00:38:13 +0100

    Working version ready to profile

--- tau.lisp
+++ tau.lisp
@@ -8,11 +8,11 @@
 
 (in-package #:tau)
 
-(defun ibm-model-1 (english-list foreign-list)
+(defun ibm-model-1 (english-list foreign-list &key (iterations 1000))
   (let* ((f-keys (list-to-hs foreign-list))
          (so-called-t (make-hash-table :test #'equal :size (hs-count f-keys)))
          (default-t-val (/ 1 (hs-count f-keys))))
-    (loop repeat 1000 ; amount of iterations
+    (loop repeat iterations ; amount of iterations
        for count = (make-hash-table :test #'equal :size (* (length english-list)
                                                            (length foreign-list)))
        for total = (make-hash-table :test #'equal :size (hs-count f-keys))
@@ -21,17 +21,17 @@
          (loop for es in english-list
             for fs in foreign-list
             do
-              (dohashset (e es)
+              (dolist (e es)
                 (setf (gethash e s-total) 0)
-                (dohashset (f fs)
+                (dolist (f fs)
                   (incf (gethash e s-total 0)
                         (gethash (cons e f) so-called-t default-t-val))))
-              (dohashset (e es)
-                (dohashset (f fs)
+              (dolist (e es)
+                (dolist (f fs)
                   (let ((val (/ (gethash (cons e f) so-called-t default-t-val)
                                 (gethash e s-total))))
                     (incf (gethash (cons e f) count 0) val)
-                    (incf (gethash f total) val)))))
+                    (incf (gethash f total 0) val)))))
          (maphash (lambda (keys _ &aux (f (cdr keys)))
                     (declare (ignore _))
                     (setf (gethash keys so-called-t)
