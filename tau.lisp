78df8a76b974661ea103e95cd6a627aba1693f88
MatthewRock <gwmaniak@gmail.com>
2017-01-08 22:44:52 +0100

    Finished first non-working draft

--- tau.lisp
+++ tau.lisp
@@ -3,21 +3,39 @@
 (in-package :cl-user)
 
 (defpackage #:tau
-  (:use #:cl #:hash-set))
+  (:use #:cl #:hash-set)
+  (:export :ibm-model-1))
 
 (in-package #:tau)
 
-(defun ibm-model-1 (e f)
-  (let ((es (list-to-hs e))
-        (fs (list-to-hs f))
-        (so-called-t (make-hash-table :test #'equal :size (hs-count fs))))
-    (loop repeat 1000
-       for count = (make-hash-table :test #'equal :size (* (hs-count es)
-                                                           (hs-count fs)))
-       for total = (make-hash-table :test #'equal :size (hs-count fs))
-       for s-total = (make-hash-table :test #'equal)
-         do
-         (dohashset (e es)
-           (hs-insert s-total )
-           (dohashset (f fs)
-             ())))))
+(defun ibm-model-1 (english-list foreign-list)
+  (let* ((f-keys (list-to-hs foreign-list))
+         (so-called-t (make-hash-table :test #'equal :size (hs-count f-keys)))
+         (default-t-val (/ 1 (hs-count f-keys))))
+    (loop repeat 1000 ; amount of iterations
+       for count = (make-hash-table :test #'equal :size (* (length english-list)
+                                                           (length foreign-list)))
+       for total = (make-hash-table :test #'equal :size (hs-count f-keys))
+       for s-total = (make-hash-table :test #'equal :size (hs-count f-keys))
+       do
+         (loop for es in english-list
+            for fs in foreign-list
+            do
+              (dohashset (e es)
+                (setf (gethash e s-total) 0)
+                (dohashset (f fs)
+                  (incf (gethash e s-total 0)
+                        (gethash (cons e f) so-called-t default-t-val))))
+              (dohashset (e es)
+                (dohashset (f fs)
+                  (let ((val (/ (gethash (cons e f) so-called-t default-t-val)
+                                (gethash e s-total))))
+                    (incf (gethash (cons e f) count 0) val)
+                    (incf (gethash f total) val)))))
+         (maphash (lambda (keys _ &aux (f (cdr keys)))
+                    (declare (ignore _))
+                    (setf (gethash keys so-called-t)
+                          (/ (gethash keys count 0)
+                             (gethash f total))))
+                  count))
+    so-called-t))
